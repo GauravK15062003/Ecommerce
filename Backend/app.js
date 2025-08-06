54e1bbb855b7823826322c2c3c2574f49fb30a18
GauravK15062003 <gauravlkr05@gmail.com>
2025-08-06 13:02:53 +0530

    Updated

--- Backend/app.js
+++ Backend/app.js
@@ -22,7 +22,7 @@ const cors = require("cors");
 
 // Add this CORS setup
 app.use(cors({
-  origin: "https://gaurav01ecommerce.netlify.app", // allow local frontend
+  origin: "https://gaurav1ecommerce.netlify.app/", // allow local frontend
   credentials: true               // allow cookies if you're using them
 }));
 
